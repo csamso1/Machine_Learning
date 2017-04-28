e21c39c10c4c2a0cc849b78a65933107b0251671
Clayton Samson <csamso1@lsu.edu>
2017-04-28 14:07:11 -0500

    Prompts user for input, clearer output

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,4 +5,10 @@ labels = [0, 0, 1, 1]
 #Creates classifier
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print(clf.predict([[160, 0]]))
+#print(clf.predict([[160, 0]]))
+weight = input("Enter the objects weight: ")
+texture = input("Enter 0 if smooth, 1 if bumpy: ")
+if clf.predict([[weight, texture]]) == 1:
+	print("It is an orange!")
+else:
+	print("It is an apple!")
