3f05cc4aabdb03e58dbb0da347799f952126ff1b
Clayton Samson <csamso1@lsu.edu>
2017-04-28 19:41:01 -0500

    Corrected Input Prompts and added comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,5 @@
 from sklearn import tree
+#Weight in grams, 1 = smooth, 0 = bumpy
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 #0 = apple, 1 = orange
 labels = [0, 0, 1, 1]
@@ -7,7 +8,7 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 #print(clf.predict([[160, 0]]))
 weight = input("Enter the objects weight: ")
-texture = input("Enter 0 if smooth, 1 if bumpy: ")
+texture = input("Enter 1 if smooth, 0 if bumpy: ")
 if clf.predict([[weight, texture]]) == 1:
 	print("It is an orange!")
 else:
